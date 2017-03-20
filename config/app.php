64dc75461a9b84f97ccadb4cbaaa0dbee5a78dd0
Justin Christenson <info@justinc.me>
2017-03-20 13:39:22 -0700

    BKME-21 Added Intervention Image lib

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Third party service providers
+         */
+        Intervention\Image\ImageServiceProvider::class
+
     ],
 
     /*
@@ -225,7 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
