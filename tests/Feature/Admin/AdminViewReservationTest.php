6f8f3cc4047dc356545dd53f614db666990d6761
Justin Christenson <info@justinc.me>
2017-03-26 14:50:51 -0700

    BKME-44 Refactor for consistency

--- tests/Feature/Admin/AdminViewReservationTest.php
+++ tests/Feature/Admin/AdminViewReservationTest.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class AdminShowReservationTest extends TestCase
+class AdminViewReservationTest extends TestCase
 {
     use DatabaseMigrations;
 
