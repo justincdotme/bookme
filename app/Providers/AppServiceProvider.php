6ea003107b40790b32ed1c53e042d7b478b36e8d
Justin Christenson <info@justinc.me>
2017-04-09 09:15:15 -0700

    BKME-81 Install Laravel Dusk

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Core\Payment\PaymentGatewayInterface;
 use App\Core\Payment\StripePaymentGateway;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Dusk\DuskServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,10 +29,17 @@ class AppServiceProvider extends ServiceProvider
         //Set locale for currency
         setlocale(LC_MONETARY, 'en_US.UTF-8');
 
+        //Configure StripePaymentGateway
         $this->app->bind(StripePaymentGateway::class, function () {
             return new StripePaymentGateway(config('services.stripe.secret'));
         });
 
+        //Bind in StripePaymentGateway
         $this->app->bind(PaymentGatewayInterface::class, StripePaymentGateway::class);
+
+        //Register Laravel Dusk
+        if ($this->app->environment('local', 'testing')) {
+            $this->app->register(DuskServiceProvider::class);
+        }
     }
 }
