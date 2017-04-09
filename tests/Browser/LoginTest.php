fe09a093dedfb11526f6a0ba9e427e14cdf7bcb1
Justin Christenson <info@justinc.me>
2017-04-09 14:45:10 -0700

    BKME-81 Found a solution to Dusk's architectural issues so now using a single DB for dev and testing (less than ideal) and then just deleting test records when test is finished.

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -2,27 +2,31 @@
 
 namespace Tests\Browser;
 
+use App\Core\User;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class LoginTest extends DuskTestCase
 {
-    use DatabaseMigrations;
-
     /**
      * @test
      * @return void
      */
     public function a_user_can_log_in()
     {
-        $this->browse(function (Browser $browser) {
+        $user = factory(User::class)->create([
+            'password' => bcrypt('secret')
+        ]);
+        $this->browse(function (Browser $browser) use ($user) {
             $browser->visit('/login')
                 ->assertSee('Login')
-                    ->type('email', 'test.user@justinc.dev')
+                    ->type('email', $user->email)
                     ->type('password', 'secret')
                     ->press('Login')
                     ->assertPathIs('/');
         });
+
+        $user->delete();
     }
 }
