47e05465187b95f459ddd6241f92602838d51470
Justin Christenson <info@justinc.me>
2017-04-16 19:23:10 -0700

    BKME-105 Working search widget

--- resources/assets/js/components/search-widget.vue
+++ resources/assets/js/components/search-widget.vue
@@ -1,17 +1,54 @@
 <template>
-    <div></div>
+    <div class="search-widget well well-lg">
+        <div class="row">
+            <div class="col-xs-12 text-center">
+                <slot name="header"></slot>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 text-right">
+                <div class="form-group">
+                    <input class="form-control" type="text" name="city" v-model="city" placeholder="city">
+                </div>
+            </div>
+            <div class="col-xs-6">
+                <div class="form-group">
+                    <select class="form-control" name="state" v-model="state" >
+                        <option v-for="(state, index) in stateList" :value="(index + 1)">{{ state }}</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 text-center">
+                <button type="button" class="btn btn-default" @click="search">Search</button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss">
     @import '../../sass/common.scss';
+    .search-widget {
+        padding: 2em 0;
+    }
 </style>
 
 <script>
     export default {
         props: [],
-        methods: {},
+        methods: {
+            search() {
+                window.bookMe.Event.fire('city-state-search', {city: this.city, state: this.state})
+            }
+        },
         data() {
-            return {}
+            return {
+                city: null,
+                state: null,
+                type: null,
+                stateList: window.states
+            }
         }
     }
 </script>
